87f969f50f1690a8da68ca170c18f8d6eb16f3a9
heavenlyalienzonearth <42529470+heavenlyalienzonearth@users.noreply.github.com>
2020-03-19 17:40:24 +0530

    Update create-rancher.sh

--- create-rancher.sh
+++ create-rancher.sh
@@ -2,7 +2,7 @@
 
 echo resourcegroup
 read resourcegroup
-azure group create $resourcegroup westeurope
+azure group create $resourcegroup eastus
 
 #azure group deployment create --name=$resourcegroup-rancher --resource-group=$resourcegroup --template-file="rancher.json" --parameters-file="azuredeploy.parameters.json"
 azure group deployment create --name=$resourcegroup-rancher --resource-group=$resourcegroup --template-file="rancher.json" 
