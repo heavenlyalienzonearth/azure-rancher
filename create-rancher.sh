460fdb82ffdeb4fcc553b6418962076bff47220d
heavenlyalienzonearth <42529470+heavenlyalienzonearth@users.noreply.github.com>
2020-03-19 19:03:42 +0530

    Update create-rancher.sh

--- create-rancher.sh
+++ create-rancher.sh
@@ -5,4 +5,4 @@
 #azure group create $resourcegroup eastus
 
 #azure group deployment create --name=$resourcegroup-rancher --resource-group=$resourcegroup --template-file="rancher.json" --parameters-file="azuredeploy.parameters.json"
-azure group deployment create --name=$resourcegroup-rancher --resource-group=$resourcegroup --template-file="rancher.json" 
+azure group deployment create --name=deepresourcegroup01-rancher --resource-group=deepresourcegroup01 --template-file="rancher.json" 
